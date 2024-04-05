a2ff079c1663871f9a360371741daa91aedb2ba0
Jhonatan Vargas <jvargas@medicated.red>
2024-04-05 14:20:39 -0600

    Feat:Creacion proyecto, configuracion de colores tailwincss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        primary: "#003763",
+        primary_2: "#0099b7",
+        secondary: "#a01779",
+        secondary_2: "#76bc21",
+        texto: "#222222",
+        texto_secundario: "#666666",
+        texto_terciario: "#AAAAAA",
+        alerta_error: "#ED3B6B",
+        alerta_success: "#76BC21",
+        alerta_info: "#0099B7",
+        alerta_warning: "#EDA93B",
+        borde_asegurame: "#DDDDDD",
+      },
     },
   },
   plugins: [],
