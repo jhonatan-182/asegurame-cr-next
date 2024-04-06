8103cefbd6986463f8b4f634ebd5da3bf1f8376d
Jhonatan Vargas <jona.vand98@gmail.com>
2024-04-05 20:39:05 -0600

    Feat: Creada pagina de cotizar de los seguros restantes

---
+++ src/app/cotizar-enfermedades-graves/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Asegurame | Seguro Médico",
+  description:
+    "Cuida de tu salud y tu bolsillo por consecuencias económicas de enfermedades graves, para que puedas concentrarte en tu recuperación. Descubre los beneficios, condiciones y aclara tus dudas de seguros médicos.",
+  keywords: [
+    "asegurame",
+    "seguros",
+    "cotiza seguro",
+    "comprar seguro",
+    "seguros en linea",
+    "seguros online",
+    "aseguradoras en costa rica",
+    "seguros costa rica",
+    "seguro médico",
+    "seguro autoexpedible enfermedades graves",
+    "ins",
+    "instituto nacional de seguros",
+    "uniserse",
+    "correduria de seguros",
+  ],
+};
+
+export default function CotizarEnfermedadesGravesPage() {
+  return (
+    <div>
+      <h1>Hello CotizarEnfermedadesGravesPage</h1>
+    </div>
+  );
+}
---
+++ src/app/cotizar-seguro-viaje/page.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Asegurame | Seguro de Viaje",
+  description:
+    "Viaja sin preocupaciones, compra tu seguro de viaje completamente en línea. Protégete de inconvenientes y recibe apoyo personalizado en tus viajes. Conoce los beneficios, requisitos y aclara tus dudas.",
+  keywords: [
+    "asegurame",
+    "seguro de viaje",
+    "asistencia al viajero",
+    "ins",
+    "instituto nacional de seguros",
+    "uniserse",
+    "correduria de seguros",
+  ],
+};
+
+export default function CotizarSeguroViajePage() {
+  return (
+    <div>
+      <h1>Hello CotizarSeguroViajePage</h1>
+    </div>
+  );
+}
